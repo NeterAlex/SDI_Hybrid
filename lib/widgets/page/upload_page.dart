028964cb2baa3ff040f39f6f59b099e7e37800d3
NeterAlex <neteralex@outlook.com>
2024-04-29 20:55:03 +0800

    feat: 引入刷新provider

--- lib/widgets/page/upload_page.dart
+++ lib/widgets/page/upload_page.dart
@@ -6,6 +6,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sdi_hybrid/common/http.dart';
+import 'package:sdi_hybrid/layout.dart';
+import 'package:sdi_hybrid/state/flow_provider.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 import '../../state/user_provider.dart';
@@ -24,6 +26,7 @@ class _UploadPageState extends State<UploadPage> {
   ];
   var uploadType = "downy";
   var imagePath = "";
+  var isUploading = false;
   late UserProvider _userProvider;
 
   @override
@@ -34,113 +37,132 @@ class _UploadPageState extends State<UploadPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BrnAppBar(
-        themeData: BrnAppBarConfig.dark(),
-        elevation: 4,
-        title: '病害识别',
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            BrnNoticeBarWithButton(
-              leftTagText: '提示',
-              leftTagBackgroundColor: const Color(0xFFE0EDFF),
-              leftTagTextColor: const Color(0xFF0984F9),
-              content: '进行识别前，请保证图片背景整洁且无明显遮挡。',
-              backgroundColor: Colors.white,
-              rightButtonBorderColor: const Color(0xFFEBFFF7),
-              rightButtonTextColor: const Color(0xFF0984F9),
-              onRightButtonTap: () {},
-            ),
-            BrnSimpleSelection.radio(
-              menuName: "选择识别模型",
-              menuKey: "model",
-              items: lists,
-              defaultValue: uploadType,
-              onSimpleSelectionChanged: (List<ItemEntity> filterParams) {
-                setState(() {
-                  uploadType = filterParams.first.key!;
-                });
-                BrnToast.show("选择了${filterParams.first.name}模型", context);
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: BrnShadowCard(
-                      padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-                      color: Colors.white,
-                      child: BrnIconButton(
-                          name: '选择图片',
-                          style: const TextStyle(
-                              fontSize: 18,
-                              color: Color.fromARGB(255, 8, 135, 235)),
-                          direction: Direction.bottom,
-                          padding: 4,
-                          iconHeight: 30,
-                          iconWidth: 30,
-                          iconWidget: const Icon(
-                            Icons.photo_size_select_actual_outlined,
-                            color: Color.fromARGB(255, 8, 135, 235),
-                          ),
-                          onTap: () async {
-                            final selectedImagePath = await _pickImage(context);
-                            setState(() {
-                              imagePath = selectedImagePath;
-                            });
-                          }),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-              child: Column(
-                children: [
-                  Row(
+    return Consumer<FlowProvider>(
+      builder: (context, flowProvider, _) {
+        return Scaffold(
+          appBar: BrnAppBar(
+            themeData: BrnAppBarConfig.dark(),
+            elevation: 4,
+            title: '病害识别',
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                BrnNoticeBarWithButton(
+                  leftTagText: '提示',
+                  leftTagBackgroundColor: const Color(0xFFE0EDFF),
+                  leftTagTextColor: const Color(0xFF0984F9),
+                  content: '进行识别前，请保证图片背景整洁且无明显遮挡。',
+                  backgroundColor: Colors.white,
+                  rightButtonBorderColor: const Color(0xFFEBFFF7),
+                  rightButtonTextColor: const Color(0xFF0984F9),
+                  onRightButtonTap: () {},
+                ),
+                BrnSimpleSelection.radio(
+                  menuName: "选择识别模型",
+                  menuKey: "model",
+                  items: lists,
+                  defaultValue: uploadType,
+                  onSimpleSelectionChanged: (List<ItemEntity> filterParams) {
+                    setState(() {
+                      uploadType = filterParams.first.key!;
+                    });
+                    BrnToast.show("选择了${filterParams.first.name}模型", context);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                  child: Row(
                     children: [
                       Expanded(
                         child: BrnShadowCard(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
                           color: Colors.white,
-                          child: imagePath == ""
-                              ? const Center(child: Text("尚未选择图片"))
-                              : Image.file(File(imagePath), fit: BoxFit.cover),
+                          child: BrnIconButton(
+                              name: '选择图片',
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Color.fromARGB(255, 8, 135, 235)),
+                              direction: Direction.bottom,
+                              padding: 4,
+                              iconHeight: 30,
+                              iconWidth: 30,
+                              iconWidget: const Icon(
+                                Icons.photo_size_select_actual_outlined,
+                                color: Color.fromARGB(255, 8, 135, 235),
+                              ),
+                              onTap: () async {
+                                final selectedImagePath =
+                                    await _pickImage(context);
+                                setState(() {
+                                  imagePath = selectedImagePath;
+                                });
+                              }),
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 30),
-                    child: BrnBigMainButton(
-                        title: "上传并识别",
-                        isEnable: imagePath != "",
-                        onTap: () async {
-                          final result = await _uploadImage(
-                              imagePath, uploadType, _userProvider.user.id);
-                          if (result) {
-                            BrnToast.show("识别成功", context);
-                            Navigator.pop(context);
-                            setState(() {
-                              imagePath = "";
-                            });
-                          } else {
-                            BrnToast.show("识别失败", context);
-                          }
-                        }),
-                  )
-                ],
-              ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: BrnShadowCard(
+                              padding:
+                                  const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              color: Colors.white,
+                              child: imagePath == ""
+                                  ? const Center(child: Text("尚未选择图片"))
+                                  : Image.file(File(imagePath),
+                                      fit: BoxFit.cover),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 30),
+                        child: BrnBigMainButton(
+                            title: "上传并识别",
+                            isEnable: imagePath != "" && isUploading != true,
+                            onTap: () async {
+                              setState(() {
+                                isUploading = true;
+                              });
+                              final result = await _uploadImage(
+                                  imagePath, uploadType, _userProvider.user.id);
+                              if (result) {
+                                BrnToast.show("识别成功", context);
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const Material(
+                                            child: LayoutPage(title: ""))));
+                                setState(() {
+                                  imagePath = "";
+                                  isUploading = false;
+                                });
+                              } else {
+                                BrnToast.show("识别失败", context);
+                                flowProvider.refreshHomePage();
+                                setState(() {
+                                  isUploading = false;
+                                });
+                              }
+                            }),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
