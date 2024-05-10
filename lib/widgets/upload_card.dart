948bcb975d5d09afaa29ece36960fdb1d90f7dce
NeterAlex <neteralex@outlook.com>
2024-05-10 20:18:06 +0800

    feat：修改UI样式

--- lib/widgets/upload_card.dart
+++ lib/widgets/upload_card.dart
@@ -14,37 +14,27 @@ class _UploadCardState extends State<UploadCard> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        BrnNoticeBarWithButton(
-          leftTagText: '提示',
-          leftTagBackgroundColor: const Color(0xFFE0EDFF),
-          leftTagTextColor: const Color(0xFF0984F9),
-          content: '进行识别前，请保证图片背景整洁',
-          backgroundColor: Colors.white,
-          rightButtonText: '已知晓',
-          rightButtonBorderColor: const Color(0xFFEBFFF7),
-          rightButtonTextColor: const Color(0xFF0984F9),
-          onRightButtonTap: () {},
-        ),
         Padding(
-          padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
           child: Row(
             children: [
               Expanded(
                 child: BrnShadowCard(
+                  circular: 8,
+                  blurRadius: 4,
                   padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-                  color: const Color.fromARGB(255, 241, 245, 255),
+                  color: const Color.fromARGB(56, 162, 255, 165),
                   child: BrnIconButton(
                       name: '病害识别',
                       style: const TextStyle(
-                          fontSize: 18,
-                          color: Color.fromARGB(255, 8, 135, 235)),
+                          fontSize: 18, color: Color.fromARGB(255, 0, 151, 87)),
                       direction: Direction.bottom,
                       padding: 4,
                       iconHeight: 30,
                       iconWidth: 30,
                       iconWidget: const Icon(
                         Icons.camera_alt_outlined,
-                        color: Color.fromARGB(255, 8, 135, 235),
+                        color: Color.fromARGB(255, 0, 151, 87),
                       ),
                       onTap: () {
                         Navigator.push(
